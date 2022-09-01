21390c3c3e65e427ebc7d2834b2b4d8c51955c1c
Aram <arampeltekian2@gmail.com>
2022-09-01 16:12:10 -0500

    commit tree 5ee8be8d3cc2b9ea53868ca2403e0169c155654b

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+//const { arrayContaining } = require("expect")
+
+const { stringMatching } = require("expect");
+
 // Code your solution here
+//const driver= ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, driverName ) {
+     for (const drivers of driverName){
+      if (driverName['bobby']=== drivers){
+        return result = drivers; 
+      }
+
+     }
+    }
+    
